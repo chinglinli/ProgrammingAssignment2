3f93b341c15b0e25638bc4181ab22697cf59d8e3
chinglinli <chinglin.li@gmail.com>
2015-07-26 19:35:44 +0800

    my 1st commit

--- cachematrix.R
+++ cachematrix.R
@@ -3,8 +3,16 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(mtx = matrix()) {
+  inverse <- NULL
+  set <- function(x) {
+    mtx <<- x;
+    inverse <<- NULL;
+  }
+  get <- function() return(mtx);
+  setinv <- function(inv) inverse <<- inv;
+  getinv <- function() return(inverse);
+  return(list(set = set, get = get, setinv = setinv, getinv = getinv))
 }
 
 
